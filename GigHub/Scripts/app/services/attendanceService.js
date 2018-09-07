984fa5cd6077ccdf77865469407ec2cc45db15a6
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-09-07 09:59:16 -0500

    Principio de responsabilidad unica para muestros archivos javascript

---
+++ GigHub/Scripts/app/services/attendanceService.js
@@ -0,0 +1,18 @@
+﻿var AttendanceService = function () {
+    var createAttendance = function (gigId, done, fail) {
+        $.post('/api/attendances', { gigId: gigId })
+            .then(done, fail);
+    };
+
+    var deleteAttendance = function (gigId, done, fail) {
+        $.ajax({
+            url: '/api/attendances/' + gigId,
+            method: 'DELETE'
+        }).then(done, fail);
+    }
+
+    return {
+        createAttendance: createAttendance,
+        deleteAttendance: deleteAttendance
+    };
+}();
