ce6538c651638fea759901027018c208239db6c9
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-09-17 08:07:44 -0500

    Se crean repositorios para la aplicacion

---
+++ GigHub/Repositories/FollowingRepository.cs
@@ -0,0 +1,23 @@
+﻿using GigHub.Models;
+using System.Linq;
+
+namespace GigHub.Repositories
+{
+    public class FollowingRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FollowingRepository(
+            ApplicationDbContext context
+        )
+        {
+            _context = context;
+        }
+
+        public Following GetFollowing(string followerId, string followeId)
+        {
+            return _context.Followings
+                        .SingleOrDefault(f => f.FolloweeId == followeId && f.FollowerId == followerId);
+        }
+    }
+}
--- GigHub/Repositories/GigRepository.cs
+++ GigHub/Repositories/GigRepository.cs
@@ -1,4 +1,5 @@
 ﻿using GigHub.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -15,6 +16,24 @@ namespace GigHub.Repositories
             _context = context;
         }
 
+        public Gig GetGig(int gigId)
+        {
+            return _context.Gigs
+                    .Include(g => g.Artist)
+                    .Include(g => g.Genre)
+                    .SingleOrDefault(g => g.Id == gigId);
+        }
+
+        public IEnumerable<Gig> GetUpcomingGigsByArtist(string artistId)
+        {
+            return _context.Gigs
+                    .Where(g => g.ArtistId == artistId &&
+                                g.DateTime > DateTime.Now &&
+                                !g.IsCanceled)
+                    .Include(g => g.Genre)
+                    .ToList();
+        }
+
         public Gig GetGigWithAttendees(int gigId)
         {
             return _context.Gigs
@@ -29,6 +48,11 @@ namespace GigHub.Repositories
                 .Include(g => g.Artist)
                 .Include(g => g.Genre)
                 .ToList();
+        }
+
+        public void Add(Gig gig)
+        {
+            _context.Gigs.Add(gig);
         }
 
     }
